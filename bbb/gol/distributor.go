18ac5f6a2f345e9955b897ff8f328f569357f16e
Gary <95489150+Gary231223123123@users.noreply.github.com>
2022-12-06 16:07:45 +0000

    Add files via upload

---
+++ bbb/gol/distributor.go
@@ -0,0 +1,324 @@
+package gol
+
+import (
+	"fmt"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+
+	"uk.ac.bris.cs/gameoflife/stubs"
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
+type distributorChannels struct {
+	events     chan<- Event
+	ioCommand  chan<- ioCommand
+	ioIdle     <-chan bool
+	ioFilename chan<- string
+	ioOutput   chan<- uint8
+	ioInput    <-chan uint8
+	keyPresses <-chan rune
+}
+
+// World type the world struct
+type World struct {
+	Width       int
+	Height      int
+	Turns       int
+	Threads     int
+	CurLattice  *Slice
+	PrevLattice *Slice
+}
+
+// Slice is a 2D slice of cells.
+type Slice struct {
+	Width   int
+	Height  int
+	Threads int
+	Cells   [][]bool
+}
+
+// distributor divides the work between workers and interacts with other goroutines.
+func distributor(p Params, c distributorChannels) {
+	worker, err := rpc.Dial("tcp", "3.83.23.107:8081")
+	if err != nil {
+		panic(err)
+	}
+	defer worker.Close()
+
+	//Create a 2D slice to store the world.
+	world := &World{
+		Height:  p.ImageHeight,
+		Width:   p.ImageWidth,
+		Turns:   p.Turns,
+		Threads: p.Threads,
+	}
+	world.CurLattice = NewSlice(p)
+	world.PrevLattice = NewSlice(p)
+
+	//load the file    world <- file
+	filename := fmt.Sprintf("%dx%d", p.ImageHeight, p.ImageWidth)
+	c.ioCommand <- ioInput
+	c.ioFilename <- filename
+	for y := 0; y < world.Height; y++ {
+		for x := 0; x < world.Width; x++ {
+			world.CurLattice.InitialCellState(x, y, <-c.ioInput == 255)
+		}
+	}
+
+	// Execute all turns of the Game of Life.
+	turn := 0
+
+	world.CellFlipped(turn, c) // send the initial state to the channel
+
+	go func(turn *int) {
+		ticker := time.NewTicker(2 * time.Second)
+		for *turn <= p.Turns {
+			select {
+			case <-ticker.C:
+				if *turn >= p.Turns-1 {
+					ticker.Stop()
+					return
+				}
+				var resp stubs.AliveCellsCountResp
+				err := worker.Call(stubs.AliveCellsCount, stubs.AliveCellsCountReq{}, &resp)
+				if err != nil {
+					panic(err)
+				}
+				c.events <- AliveCellsCount{
+					CompletedTurns: resp.Turn,
+					CellsCount:     resp.Count,
+				}
+			case key := <-c.keyPresses:
+				var resp stubs.KeyPressResp
+				err := worker.Call(stubs.KeyPress, stubs.KeyPressReq{Key: key}, &resp)
+				if err != nil {
+					panic(err)
+				}
+				switch key {
+				case 'p':
+					c.events <- StateChange{
+						CompletedTurns: resp.Turn,
+						NewState:       Paused,
+					}
+					for {
+						key := <-c.keyPresses
+						if key == 'p' {
+							err := worker.Call(stubs.KeyPress, stubs.KeyPressReq{Key: key}, &resp)
+							if err != nil {
+								panic(err)
+							}
+							c.events <- StateChange{
+								CompletedTurns: resp.Turn,
+								NewState:       Executing,
+							}
+							fmt.Println("Continuing")
+							break
+						}
+					}
+				case 'q':
+					c.events <- StateChange{
+						CompletedTurns: resp.Turn,
+						NewState:       Quitting,
+					}
+					close(c.events)
+					os.Exit(0)
+				case 's':
+					world.CurLattice.Cells = resp.World.Cells
+					world.GenerateFile(resp.Turn, p, c)
+				case 'k':
+					world.CurLattice.Cells = resp.World.Cells
+					world.GenerateFile(resp.Turn, p, c)
+					if key == 'k' {
+						c.events <- StateChange{
+							CompletedTurns: resp.Turn,
+							NewState:       Quitting,
+						}
+						close(c.events)
+						os.Exit(0)
+					}
+				}
+			}
+		}
+	}(&turn)
+
+	req := stubs.GameOfLifeReq{
+		World: stubs.World{
+			Width:   world.Width,
+			Height:  world.Height,
+			Threads: world.Threads,
+			Cells:   world.CurLattice.Cells,
+			Turns:   world.Turns,
+		},
+	}
+
+	var resp stubs.GameOfLifeResp
+	err = worker.Call(stubs.GameOfLife, req, &resp)
+	if err != nil {
+		panic(err)
+	}
+
+	turn = resp.Turns
+	world.CurLattice.Cells = resp.World.Cells
+
+	//Report the final state using FinalTurnCompleteEvent.
+	var cells []util.Cell
+	for y := 0; y < world.Height; y++ {
+		for x := 0; x < world.Width; x++ {
+			if world.CurLattice.CellState(x, y) {
+				cells = append(cells, util.Cell{X: x, Y: y})
+			}
+		}
+	}
+	c.events <- FinalTurnComplete{ //send to channel
+		CompletedTurns: world.Turns,
+		Alive:          cells,
+	}
+	//save the file and output.
+	filename = fmt.Sprintf("%dx%dx%d", p.ImageHeight, p.ImageWidth, turn)
+	c.ioCommand <- ioOutput
+	c.ioFilename <- filename
+	for y := 0; y < world.Height; y++ {
+		for x := 0; x < world.Width; x++ {
+			var value uint8
+			if world.CurLattice.CellState(x, y) {
+				value = 255 //alive
+			} else {
+				value = 0 //dead
+			}
+			c.ioOutput <- value
+		}
+	}
+	c.events <- ImageOutputComplete{
+		Filename:       filename,
+		CompletedTurns: turn,
+	}
+
+	// Make sure that the Io has finished any output before exiting.
+	c.ioCommand <- ioCheckIdle
+	<-c.ioIdle
+	c.events <- StateChange{turn, Quitting}
+	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
+	close(c.events)
+}
+
+// NewSlice creates a new slice.
+func NewSlice(p Params) *Slice {
+	slice := &Slice{
+		Width:   p.ImageWidth,
+		Height:  p.ImageHeight,
+		Threads: p.Threads,
+	}
+	slice.Cells = make([][]bool, slice.Height)
+	for i := range slice.Cells {
+		slice.Cells[i] = make([]bool, slice.Width)
+	}
+	return slice
+}
+
+// Run runs the distributor. (go by example mockup)
+func (w *World) Run() {
+	var wg sync.WaitGroup
+	for y := 0; y < w.Height; {
+		for i := 0; i < w.Threads; i++ {
+			wg.Add(1)
+			go func(wg *sync.WaitGroup, y int) {
+				defer wg.Done()
+				for x := 0; x < w.Width; x++ {
+					w.PrevLattice.InitialCellState(x, y, w.CurLattice.NextStep(x, y))
+				}
+			}(&wg, y)
+			y++
+			if y == w.Height {
+				break
+			}
+		}
+	}
+	wg.Wait()
+	w.PrevLattice, w.CurLattice = w.CurLattice, w.PrevLattice
+}
+
+// CellFlipped send the flipped cell to the channel.
+func (w *World) CellFlipped(turn int, c distributorChannels) {
+	for y := 0; y < w.Height; y++ {
+		for x := 0; x < w.Width; x++ {
+			if w.CurLattice.CellState(x, y) != w.PrevLattice.CellState(x, y) { //cell position changed
+				c.events <- CellFlipped{
+					CompletedTurns: turn, Cell: util.Cell{
+						X: x,
+						Y: y,
+					},
+				}
+			}
+		}
+	}
+}
+
+func (s *Slice) InitialCellState(x, y int, alive bool) {
+	if x >= 0 && x < s.Width && y >= 0 && y < s.Height {
+		s.Cells[x][y] = alive
+	}
+}
+
+// CellState returns the position of the cell .
+func (s *Slice) CellState(x, y int) bool {
+	if x >= 0 && x < s.Width && y >= 0 && y < s.Height {
+		return s.Cells[x][y]
+	} else {
+		if x == s.Width || x == (-1) {
+			x = (x + s.Width) % s.Width //closed domain
+		}
+		if y == s.Height || y == (-1) {
+			y = (y + s.Height) % s.Height //closed domain
+		}
+		return s.Cells[x][y]
+	}
+}
+
+func (s *Slice) NextStep(x, y int) bool {
+	alive := 0
+	for row := -1; row <= 1; row++ {
+		for column := -1; column <= 1; column++ {
+			if s.CellState(x+row, y+column) && (row != 0 || column != 0) {
+				alive++
+			}
+		}
+	}
+	return alive == 3 || (s.CellState(x, y) && alive == 2)
+}
+
+func (w *World) GenerateFile(turn int, p Params, c distributorChannels) {
+	filename := fmt.Sprintf("%dx%dx%d-%d", p.ImageHeight, p.ImageWidth, p.Threads, turn)
+	c.events <- ImageOutputComplete{ //generate file
+		Filename:       filename,
+		CompletedTurns: turn,
+	}
+}
+
+// GetAliveCellsCount get the alive cells number.
+func (w *World) GetAliveCellsCount() int {
+	var count = 0
+	for y := 0; y < w.Height; y++ {
+		for x := 0; x < w.Width; x++ {
+			if w.CurLattice.CellState(x, y) {
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+// GetCellsChanged get the cells that are changed.
+func (w *World) GetCellsChanged() []util.Cell {
+	var cells []util.Cell
+	for y := 0; y < w.Height; y++ {
+		for x := 0; x < w.Width; x++ {
+			if w.CurLattice.CellState(x, y) != w.PrevLattice.CellState(x, y) {
+				cells = append(cells, util.Cell{X: x, Y: y})
+			}
+		}
+	}
+	return cells
+}
---
+++ bbb/gol_worker/gol_worker.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"net/rpc"
+	"os"
+	"sync"
+
+	"uk.ac.bris.cs/gameoflife/gol"
+	"uk.ac.bris.cs/gameoflife/stubs"
+)
+
+type GolWorker struct {
+	world               *gol.World
+	pasused             bool
+	pauseChannel        chan bool
+	quitChannel         chan bool
+	DistributorQuitChan chan bool
+	currentTurn         int
+	mutex               sync.Mutex
+}
+
+// NewGolWorker creates a new worker
+func NewGolWorker() *GolWorker {
+	return &GolWorker{
+		world:               nil,
+		pasused:             false,
+		pauseChannel:        make(chan bool),
+		quitChannel:         make(chan bool),
+		DistributorQuitChan: make(chan bool),
+		currentTurn:         0,
+		mutex:               sync.Mutex{},
+	}
+}
+
+// GameOfLife is a method
+func (g *GolWorker) GameOfLife(req stubs.GameOfLifeReq, resp *stubs.GameOfLifeResp) error {
+	p := gol.Params{
+		ImageHeight: req.World.Height,
+		ImageWidth:  req.World.Width,
+		Turns:       req.World.Turns,
+		Threads:     req.World.Threads,
+	}
+
+	g.world = &gol.World{
+		Width:       req.World.Width,
+		Height:      req.World.Height,
+		Turns:       req.World.Turns,
+		Threads:     req.World.Threads,
+		CurLattice:  gol.NewSlice(p),
+		PrevLattice: gol.NewSlice(p),
+	}
+
+	for i := range req.World.Cells {
+		copy(g.world.CurLattice.Cells[i], req.World.Cells[i])
+	}
+
+	for turn := 0; turn <= g.world.Turns; turn++ {
+		select {
+		case <-g.pauseChannel:
+			fmt.Printf("Turn %d Paused\n", turn)
+			for {
+				<-g.pauseChannel
+				fmt.Printf("Turn %d Resumed\n", turn)
+				break
+			}
+		case <-g.quitChannel:
+			fmt.Printf("Turn %d Quit\n", turn)
+			os.Exit(0)
+		case <-g.DistributorQuitChan:
+			g.world = nil
+			g.currentTurn = 0
+			g.pasused = false
+			fmt.Printf("Turn %d Local Quit\n", turn)
+			resp.Turns = g.currentTurn
+			return nil
+		default:
+			g.mutex.Lock()
+			g.currentTurn = turn
+			g.Run()
+			g.mutex.Unlock()
+		}
+	}
+
+	resp.Turns = g.currentTurn
+	resp.World.Cells = g.world.PrevLattice.Cells
+	fmt.Printf("Turn %d Complete\n", g.currentTurn)
+
+	return nil
+}
+
+// Run starts the worker
+func (g *GolWorker) Run() {
+	var wg sync.WaitGroup
+	for y := 0; y < g.world.Height; y++ {
+		for i := 0; i < g.world.Threads; i++ {
+			wg.Add(1)
+			go func(y int, wg *sync.WaitGroup) {
+				defer wg.Done()
+				for x := 0; x < g.world.Width; x++ {
+					g.world.PrevLattice.InitialCellState(x, y, g.world.CurLattice.NextStep(x, y))
+				}
+			}(y, &wg)
+		}
+	}
+
+	wg.Wait()
+	g.world.PrevLattice, g.world.CurLattice = g.world.CurLattice, g.world.PrevLattice
+}
+
+// AliveCellsCount is the AliveCellsCount method
+func (g *GolWorker) AliveCellsCount(req stubs.AliveCellsCountReq, resp *stubs.AliveCellsCountResp) error {
+	g.mutex.Lock()
+	defer g.mutex.Unlock()
+
+	resp.Count = g.world.GetAliveCellsCount()
+	resp.Turn = g.currentTurn
+	return nil
+}
+
+// KeyPress is the KeyPress method
+func (g *GolWorker) KeyPress(req stubs.KeyPressReq, resp *stubs.KeyPressResp) error {
+	resp.Turn = g.currentTurn
+	switch req.Key {
+	case 'p':
+		g.pauseChannel <- true
+		g.pasused = !g.pasused
+		resp.Pause = g.pasused
+	case 'q':
+		g.DistributorQuitChan <- true
+	case 's':
+		resp.World.Cells = g.world.CurLattice.Cells
+	case 'k':
+		resp.World.Cells = g.world.CurLattice.Cells
+		g.quitChannel <- true
+	}
+
+	return nil
+}
+
+func main() {
+	port := flag.String("port", "8081", "port listen on")
+	flag.Parse()
+	listener, err := net.Listen("tcp", ":"+*port)
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	fmt.Printf("Listening on port %s\n", *port)
+
+	rpc.Register(NewGolWorker())
+	rpc.Accept(listener)
+}
